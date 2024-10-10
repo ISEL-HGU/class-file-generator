bbc47d743de7496bd35929abe1dde8c0a5ad1ae0
Hahyun Baek <hahyun8587@gmail.com>
2024-10-10 13:49:54 +0900

    feat: add format checking feature in read()

--- class-file-generator/src/main/java/edu/handong/csee/isel/cfg/JsonReader.java
+++ class-file-generator/src/main/java/edu/handong/csee/isel/cfg/JsonReader.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.logging.Logger;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
@@ -18,21 +21,50 @@ public class JsonReader {
 
     /**
      * Reads <code>jsonFile</code>.
-     * @param jsonFile json <code>File</code> to read
+     * @param jsonFile json file to read
      * @return <code>ClassInfo</code> instance
      * @throws FileNotFoundException if the file does not exist, is a directory 
      *      rather than a regular file, or for some other reason cannot be 
      *      opened for reading
-     * @throws JsonSyntaxException if json is not a valid representation for 
-     *      an object of <code>ClassInfo</code>
+     * @throws JsonSyntaxException if the file is not a json file, contains 
+     *      malformed json element, or is not a valid representation for an 
+     *      object of <code>ClassInfo</code>
      * @throws JsonIOException if there was a problem reading from the Reader
-     * @throws IOException If an I/O error occurs
+     * @throws IllegalAccessException if the getter methods of created 
+     *      <code>ClassInfo</code> object are inaccessible
+     * @throws InvocationTargetException if the getter methods of created 
+     *      <code>ClassInfo</code> object throw exceptions
      */
     public ClassInfo read(File jsonFile) throws FileNotFoundException, 
-            JsonSyntaxException, JsonIOException, IOException {
+            JsonSyntaxException, JsonIOException, IllegalAccessException,
+            InvocationTargetException {     
+        ClassInfo info = null;   
+        
         try (BufferedReader br 
                 = new BufferedReader(new FileReader(jsonFile))) {
-            return gson.fromJson(br, ClassInfo.class);
+            if (!jsonFile.getPath().endsWith(".json")) {
+                throw new JsonSyntaxException("The file is not a json file.");
+            }
+
+            info = gson.fromJson(br, ClassInfo.class);
+            
+            for (Method method : info.getClass().getMethods()) {
+                if (method.getName().startsWith("get") 
+                        && method.invoke(info) == null) { 
+                    throw new JsonSyntaxException(
+                            "The json file does not follow expected format.");
+                }
+            }
+        } catch (IOException e) {
+            if (e instanceof FileNotFoundException) {
+                throw (FileNotFoundException) e;
+            }
+
+            Logger.getLogger(getClass().getName())
+                  .warning("Cannot close the reader after reading " 
+                           + jsonFile.getPath() + ".");
         }
+
+        return info;
     }
 }
