7a30a72eca8b8d1a3e886958f62d6613a326326b
Hahyun Baek <hahyun8587@gmail.com>
2024-10-31 12:43:06 +0900

    add FileExtensionException

---
+++ class-file-generator/src/main/java/edu/handong/csee/isel/cfg/FileExtensionException.java
@@ -0,0 +1,9 @@
+package edu.handong.csee.isel.cfg;
+
+import java.io.IOException;
+
+public class FileExtensionException extends IOException {
+    public FileExtensionException(String message) {
+        super(message);
+    }
+}
