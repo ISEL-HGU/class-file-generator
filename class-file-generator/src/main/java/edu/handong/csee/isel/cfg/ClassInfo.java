df330dc7d4cd24c44fc5f76a3daf7fa2e87a8f61
Hahyun Baek <hahyun8587@gmail.com>
2024-10-31 12:42:15 +0900

    refactor: change exception handling

--- class-file-generator/src/main/java/edu/handong/csee/isel/cfg/ClassInfo.java
+++ class-file-generator/src/main/java/edu/handong/csee/isel/cfg/ClassInfo.java
@@ -3,11 +3,14 @@ package edu.handong.csee.isel.cfg;
 import java.lang.IllegalAccessException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * Class that contains information for creating class file.
  */
 public class ClassInfo {  
+    private static Random rnd = new Random();
+
     private int[] bytecode;
     private String classname;
     private String methodname;
@@ -15,15 +18,23 @@ public class ClassInfo {
     private String packagename;
     private int version;
 
-    public ClassInfo(int[] bytecode, String classname, 
-                     String methodname, String methodDesc, 
-                     String packagename, int version) {
-        this.bytecode = bytecode;
+    public ClassInfo() {
+        version = 55;
+        packagename = "gen";
+        classname = "GeneratedClass" + rnd.nextInt();
+        methodname = "solve";
+        methodDesc = "()V";
+        bytecode = new int[] { 177 };
+    }
+
+    public ClassInfo(int version, String packagename, String classname, 
+                     String methodname, String methodDesc, int[] bytecode) {
+        this.version = version;
+        this.packagename = packagename;
         this.classname = classname;
         this.methodname = methodname;
         this.methodDesc = methodDesc;
-        this.packagename = packagename;
-        this.version = version;
+        this.bytecode = bytecode;
     }
 
     public int[] getBytecode() {
@@ -58,7 +69,9 @@ public class ClassInfo {
         
         for (Field field : getClass().getDeclaredFields()) {
             try {
-                Object val = field.get(this);
+                Object val;
+                
+                val = field.get(this);
 
                 if (val instanceof int[]) {
                     if (!Arrays.equals((int []) val, (int []) field.get(obj))) {
@@ -70,7 +83,10 @@ public class ClassInfo {
                     }
                 }
             } catch (IllegalAccessException e) {
+                System.err.println(e.getMessage());
                 e.printStackTrace();
+
+                return false;
             }
         }
         
